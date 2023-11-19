96adebd22a7a66e86e8a8211ec59855bcdf24936
tomek <2002tomo@gmail.com>
2023-11-19 22:19:53 +0100

    udało się wyswietlić tytuł, edytować a takze wrocic do głównego ekranu

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -13,6 +13,13 @@ class StartScreen extends StatefulWidget{
 
 class _StartScreenState extends State<StartScreen> {
   bool showBigButton = true;
+  String globalTitle = '';
+
+  void updateDisplayedTitle(String newTitle) {
+    setState(() {
+      globalTitle = newTitle;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +37,10 @@ class _StartScreenState extends State<StartScreen> {
       body: Center(
         child: showBigButton ? 
           StartButton(width: 350, height: 630, onShowBigButtonChanged: (newValue) {
-            setState(() {showBigButton = newValue;});},): 
+            setState(() {showBigButton = newValue;});},updateDisplayedTitle: updateDisplayedTitle,) :
             Timer(onShowBigButtonChanged: (newValue) {
-            setState(() {showBigButton = newValue;});},)
+            setState(() {showBigButton = newValue;});},updateDisplayedTitle: updateDisplayedTitle,
+                  globalTitle: globalTitle,)
             )
     );
   }
@@ -40,16 +48,30 @@ class _StartScreenState extends State<StartScreen> {
 
 class Timer extends StatefulWidget {
   final Function(bool) onShowBigButtonChanged;
-
-  const Timer({super.key, required this.onShowBigButtonChanged});
+  final Function(String) updateDisplayedTitle;
+  final String globalTitle;
+  const Timer({super.key, required this.onShowBigButtonChanged, required this.updateDisplayedTitle,
+  required this.globalTitle,});
   
   @override
   State<StatefulWidget> createState() => _Timer();
 }
 
 class _Timer extends State<Timer> {
+
+  final TextEditingController _textEditingController = TextEditingController();
+  DateTime date = DateTime(2016, 10, 26);
+
+
+  @override
+  void dispose(){
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    String globalTitle = widget.globalTitle;
    return Scaffold(
       backgroundColor:const Color.fromARGB(255, 28, 28, 30),
       body: SizedBox(
@@ -71,7 +93,7 @@ class _Timer extends State<Timer> {
                     ),
                     TextButton(
                       onPressed: () {
-                        widget.onShowBigButtonChanged(true);
+                        _bottomSheetPopUp(context);
                       },
                       child: const Text(
                         'Edit',
@@ -83,11 +105,10 @@ class _Timer extends State<Timer> {
                     ),
                   ],
                 ),
-                const SizedBox(
+                SizedBox(
                   height: 80,
-                  child: Text(
-                    'Tytuł',
-                    style: TextStyle(
+                  child: Text(globalTitle,
+                    style: const TextStyle(
                       fontSize: 32,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
@@ -99,14 +120,147 @@ class _Timer extends State<Timer> {
       ),
     );
   }
+  Future _bottomSheetPopUp(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(12))),
+      backgroundColor: const Color.fromARGB(255, 28, 28, 30),
+      isDismissible: false,
+      isScrollControlled: true,
+      builder: (context) => SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: SizedBox(
+            height: 785, // Adjust the height as needed
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        _textEditingController.clear();
+                      },
+                      child: const Text(
+                        'Cancel',
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        widget.updateDisplayedTitle(_textEditingController.text);
+                        Navigator.of(context).pop();
+                        widget.onShowBigButtonChanged(false);
+                      },
+                      child: const Text(
+                        'Done',
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 80,
+                  child: Text(
+                    'Edytuj licznik',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 350,
+                  height: 40,
+                    child: CupertinoTextField(
+                      controller: _textEditingController,
+                      placeholder: 'Title',
+                      placeholderStyle: const TextStyle(
+                        color: Color.fromARGB(255, 153, 153, 160)
+                      ),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 44, 44, 46),
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                ),
+                SizedBox(
+                  height: 200,
+                  width: 360,
+                  child: CupertinoTheme(
+                    data: const CupertinoThemeData(
+                      brightness: Brightness.dark),
+                    child: CupertinoDatePicker(
+                      minimumDate: DateTime.now(),
+                      initialDateTime: DateTime.now(),
+                      mode: CupertinoDatePickerMode.date,
+                      onDateTimeChanged: (DateTime newDate) {
+                        setState(() => date = newDate);
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 350,
+                  height: 40,
+                  child: InkWell(
+                    enableFeedback: false,
+                    onTap: () {
+                      widget.onShowBigButtonChanged(true);
+                      Navigator.of(context).pop();
+                    },
+                  child: OutlinedButton(
+                    onPressed: null,
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),       
+                      ),
+                      backgroundColor: const Color.fromARGB(255, 44, 44, 46)
+                    ),
+                    child: const Text(
+                      'Delete',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 0, 0),
+                        fontSize: 15, // Set text color to transparent
+                        ),
+                      ),
+                    ),
+                  )
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class StartButton extends StatefulWidget {
   final double width;
   final double height;
   final Function(bool) onShowBigButtonChanged;
+  final Function(String) updateDisplayedTitle;
 
-  const StartButton({super.key, required this.height, required this.width, required this.onShowBigButtonChanged,});
+  const StartButton({super.key, required this.height, required this.width, 
+  required this.onShowBigButtonChanged, 
+  required this.updateDisplayedTitle});
 
   @override
   State<StartButton> createState() => _StartButtonState();
@@ -115,8 +269,6 @@ class StartButton extends StatefulWidget {
 class _StartButtonState extends State<StartButton> {
   final TextEditingController _textEditingController = TextEditingController();
   DateTime date = DateTime(2016, 10, 26);
-  String globalTitle = '';
-
 
 
   @override
@@ -188,7 +340,7 @@ class _StartButtonState extends State<StartButton> {
                       onPressed: () {
                         Navigator.of(context).pop();
                         _textEditingController.clear();
-                        globalTitle = '';
+                        widget.updateDisplayedTitle('');
                       },
                       child: const Text(
                         'Cancel',
@@ -200,7 +352,7 @@ class _StartButtonState extends State<StartButton> {
                     ),
                     TextButton(
                       onPressed: () {
-                        globalTitle = _textEditingController.text;
+                        widget.updateDisplayedTitle(_textEditingController.text);
                         Navigator.of(context).pop();
                         _textEditingController.clear();
                         widget.onShowBigButtonChanged(false);
@@ -218,7 +370,7 @@ class _StartButtonState extends State<StartButton> {
                 const SizedBox(
                   height: 80,
                   child: Text(
-                    'Edytuj licznik',
+                    'Dodaj licznik',
                     style: TextStyle(
                       fontSize: 32,
                       fontWeight: FontWeight.bold,
